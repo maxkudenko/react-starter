d012445ae51fbcf7d727c5dc1d6556df9e90efdd
Maksym Kudenko <maxkudenko@gmail.com>
2022-12-07 16:12:57 +0100

    Add absolute imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
+import { App } from "App";
 import { StrictMode } from "react";
 import * as ReactDOM from "react-dom/client"
-import { App } from "./App"
 
 const container = document.getElementById("root")
 if (!container) throw new Error('Failed to find the root element');
