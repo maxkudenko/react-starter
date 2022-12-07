4de6bd1674e5b2a7c06a22bc5f61ec0788dc6cf0
Maksym Kudenko <maxkudenko@gmail.com>
2022-12-07 16:08:19 +0100

    Сlear the project from a redundant logic

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,4 @@
+// These type declarations add support for importing resource files such as bmp, gif, jpeg, jpg, png, webp, and svg.
+// It also adds support for importing CSS Modules. This relates to import of files with .module.css,.module.scss, and .module.sass extensions.
+// Do not remove the react-app-env.d.ts file to avoid errors.
 /// <reference types="react-scripts" />
