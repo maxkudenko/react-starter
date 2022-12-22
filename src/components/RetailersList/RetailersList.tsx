e7826f9ebdb5625d8d08f77499d3a6e174dcdfe7
Maksym Kudenko <maksym.kudenko@valtech.com>
2022-12-22 11:03:02 +0100

    Select a retailer by default

--- src/components/RetailersList/RetailersList.tsx
+++ src/components/RetailersList/RetailersList.tsx
@@ -1,5 +1,6 @@
 import { Box } from '@chakra-ui/react'
-import { useSetAtom } from 'jotai'
+import { useAtom } from 'jotai'
+import { useEffect } from 'react'
 import { useQuery } from 'react-query'
 
 import { fetchRetailers } from 'api/retailersAPI'
@@ -8,9 +9,17 @@ import RetailerCard from 'components/RetailerCard'
 import { Retailer } from 'types/retailerTypes'
 
 const RetailersList = () => {
-  const setSelectedRetailerId = useSetAtom(selectedRetailerIdAtom)
+  const [selectedRetailerId, setSelectedRetailerId] = useAtom(
+    selectedRetailerIdAtom
+  )
 
   const { data: retailers } = useQuery<Retailer[]>('retailers', fetchRetailers)
+
+  useEffect(() => {
+    if (retailers && !selectedRetailerId) {
+      setSelectedRetailerId(retailers?.[0]?.id || '')
+    }
+  }, [retailers, selectedRetailerId])
 
   return (
     <Box w="30%" p="5" cursor="pointer">
